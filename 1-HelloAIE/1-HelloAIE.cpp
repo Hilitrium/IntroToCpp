19047c848e27f85643ab7de0c8371a1311bda42b
s179063 <s179063@SEA-8D0AB3>
2017-08-14 12:59:52 -0700

    Add project files.

---
+++ 1-HelloAIE/1-HelloAIE.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Hello" << std::endl << "World" << std::endl;
+
+	system("pause");
+	
+	std::cout << "Exist - Avenged Sevenfold" << std::endl;
+	std::cout << "Indestructible - Disturbed" << std::endl;
+	std::cout << "Sounds Of Silence - Disturbed" << std::endl;
+	std::cout << "Higher - Avenged Sevenfold" << std::endl;
+	std::cout << "Patience - Demon Hunter" << std::endl;
+
+	system("pause");
+	
+	std::cout << "0" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "5" << std::endl;
+
+	system("pause");
+
+	std::cout << "5" << std::endl;
+	std::cout << "4" << std::endl;
+	std::cout << "3" << std::endl;
+	std::cout << "2" << std::endl;
+	std::cout << "1" << std::endl;
+	std::cout << "0" << std::endl;
+
+	system("pause");
+// the std::endl part ends the line and sets the next one to the next line down
+}
